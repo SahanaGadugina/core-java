19cafbfa61636300511750527b22688e98c81e54
SahanaGadugina <149233688+SahanaGadugina@users.noreply.github.com>
2023-10-31 12:01:39 +0530

    Add files via upload

---
+++ ShowRoomMain.java
@@ -0,0 +1,100 @@
+class ShowRoomMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting main in ShowRoomMain");
+		ShowRoom showroom=new ShowRoom();
+		System.out.println("Showroom name:"+showroom.name);
+		System.out.println("Showroom color:"+showroom.color);
+		System.out.println("Showroom brand:"+showroom.brand);
+		System.out.println("Showroom noOfBikes:"+showroom.noOfBikes);
+		System.out.println("Showroom location:"+showroom.location);
+		System.out.println("Showroom area:"+showroom.area);
+		System.out.println("Showroom cost:"+showroom.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom.noOfEmployees);
+		
+		ShowRoom showroom1=new ShowRoom("Racer");
+		System.out.println("Showroom name:"+showroom1.name);
+		System.out.println("Showroom color:"+showroom1.color);
+		System.out.println("Showroom brand:"+showroom1.brand);
+		System.out.println("Showroom noOfBikes:"+showroom1.noOfBikes);
+		System.out.println("Showroom location:"+showroom1.location);
+		System.out.println("Showroom area:"+showroom1.area);
+		System.out.println("Showroom cost:"+showroom1.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom1.noOfEmployees);
+		
+		ShowRoom showroom2=new ShowRoom("Racer","black");
+		System.out.println("Showroom name:"+showroom2.name);
+		System.out.println("Showroom color:"+showroom2.color);
+		System.out.println("Showroom brand:"+showroom2.brand);
+		System.out.println("Showroom noOfBikes:"+showroom2.noOfBikes);
+		System.out.println("Showroom location:"+showroom2.location);
+		System.out.println("Showroom area:"+showroom2.area);
+		System.out.println("Showroom cost:"+showroom2.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom2.noOfEmployees);
+		
+		ShowRoom showroom3=new ShowRoom("Racer","black","Honda");
+		System.out.println("Showroom name:"+showroom3.name);
+		System.out.println("Showroom color:"+showroom3.color);
+		System.out.println("Showroom brand:"+showroom3.brand);
+		System.out.println("Showroom noOfBikes:"+showroom3.noOfBikes);
+		System.out.println("Showroom location:"+showroom3.location);
+		System.out.println("Showroom area:"+showroom3.area);
+		System.out.println("Showroom cost:"+showroom3.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom3.noOfEmployees);
+		
+		ShowRoom showroom4=new ShowRoom("Racer","black","Honda",10);
+		System.out.println("Showroom name:"+showroom4.name);
+		System.out.println("Showroom color:"+showroom4.color);
+		System.out.println("Showroom brand:"+showroom4.brand);
+		System.out.println("Showroom noOfBikes:"+showroom4.noOfBikes);
+		System.out.println("Showroom location:"+showroom4.location);
+		System.out.println("Showroom area:"+showroom4.area);
+		System.out.println("Showroom cost:"+showroom4.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom4.noOfEmployees);
+		
+		ShowRoom showroom5=new ShowRoom("Racer","black","Honda",10,"Bangalore");
+		System.out.println("Showroom name:"+showroom5.name);
+		System.out.println("Showroom color:"+showroom5.color);
+		System.out.println("Showroom brand:"+showroom5.brand);
+		System.out.println("Showroom noOfBikes:"+showroom5.noOfBikes);
+		System.out.println("Showroom location:"+showroom5.location);
+		System.out.println("Showroom area:"+showroom5.area);
+		System.out.println("Showroom cost:"+showroom5.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom5.noOfEmployees);
+		
+		ShowRoom showroom6=new ShowRoom("Racer","black","Honda",10,"Bangalore","BTM-Layout");
+		System.out.println("Showroom name:"+showroom6.name);
+		System.out.println("Showroom color:"+showroom6.color);
+		System.out.println("Showroom brand:"+showroom6.brand);
+		System.out.println("Showroom noOfBikes:"+showroom6.noOfBikes);
+		System.out.println("Showroom location:"+showroom6.location);
+		System.out.println("Showroom area:"+showroom6.area);
+		System.out.println("Showroom cost:"+showroom6.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom6.noOfEmployees);
+		
+		ShowRoom showroom7=new ShowRoom("Racer","black","Honda",10,"Bangalore","BTM-Layout",150000);
+		System.out.println("Showroom name:"+showroom7.name);
+		System.out.println("Showroom color:"+showroom7.color);
+		System.out.println("Showroom brand:"+showroom7.brand);
+		System.out.println("Showroom noOfBikes:"+showroom7.noOfBikes);
+		System.out.println("Showroom location:"+showroom7.location);
+		System.out.println("Showroom area:"+showroom7.area);
+		System.out.println("Showroom cost:"+showroom7.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom7.noOfEmployees);
+		
+		ShowRoom showroom8=new ShowRoom("Racer","black","Honda",10,"Bangalore","BTM-Layout",150000,20);
+		System.out.println("Showroom name:"+showroom8.name);
+		System.out.println("Showroom color:"+showroom8.color);
+		System.out.println("Showroom brand:"+showroom8.brand);
+		System.out.println("Showroom noOfBikes:"+showroom8.noOfBikes);
+		System.out.println("Showroom location:"+showroom8.location);
+		System.out.println("Showroom area:"+showroom8.area);
+		System.out.println("Showroom cost:"+showroom8.cost);
+		System.out.println("Showroom noOfEmployees:"+showroom8.noOfEmployees);
+		
+	}
+}
+
+		
+		
