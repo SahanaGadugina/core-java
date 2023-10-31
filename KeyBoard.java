56e268ff662051f645c1f89db4ba1676af4f1099
SahanaGadugina <149233688+SahanaGadugina@users.noreply.github.com>
2023-10-31 11:59:09 +0530

    Add files via upload

---
+++ KeyBoard.java
@@ -0,0 +1,63 @@
+class KeyBoard
+{
+	String brand;
+	String color;
+	int noOfKeys;
+	String type;
+	double cost;
+	KeyBoard()
+	{
+		System.out.println("Default constructor is created");
+	}
+	KeyBoard(String brand)
+	{
+		this.brand=brand;
+	}
+	KeyBoard(String brand,String color)
+	{
+		this.brand=brand;
+		this.color=color;
+	}
+	KeyBoard(String brand,String color,int noOfKeys)
+	{
+		this.brand=brand;
+		this.color=color;
+		this.noOfKeys=noOfKeys;
+	}
+	KeyBoard(String brand,String color,int noOfKeys,String type)
+	{
+		this.brand=brand;
+		this.color=color;
+		this.noOfKeys=noOfKeys;
+		this.type=type;
+	}
+	KeyBoard(String brand,String color,int noOfKeys,String type,double cost)
+	{
+		this.brand=brand;
+		this.color=color;
+		this.noOfKeys=noOfKeys;
+		this.type=type;
+		this.cost=cost;
+	}
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
