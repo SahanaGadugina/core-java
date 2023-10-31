4529d23413064ea081eda4e2fea3b66fab01b726
SahanaGadugina <149233688+SahanaGadugina@users.noreply.github.com>
2023-10-31 12:00:18 +0530

    Add files via upload

---
+++ MatchBoxMain.java
@@ -0,0 +1,91 @@
+class MatchBoxMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting main in MatchBoxMain");
+		MatchBox matchbox=new MatchBox();
+		System.out.println("Matchbox brand:" +matchbox.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox.length);
+		System.out.println("Matchbox price:" +matchbox.price);
+		System.out.println("Matchbox size:" +matchbox.size);
+		System.out.println("Matchbox weight:" +matchbox.weight);
+		System.out.println("Matchbox type:" +matchbox.type);
+		
+		MatchBox matchbox1=new MatchBox("Kamal");
+		System.out.println("Matchbox brand:" +matchbox1.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox1.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox1.length);
+		System.out.println("Matchbox price:" +matchbox1.price);
+		System.out.println("Matchbox size:" +matchbox1.size);
+		System.out.println("Matchbox weight:" +matchbox1.weight);
+		System.out.println("Matchbox type:" +matchbox1.type);
+		
+		MatchBox matchbox2=new MatchBox("kamal",100);
+		System.out.println("Matchbox brand:" +matchbox2.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox2.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox2.length);
+		System.out.println("Matchbox price:" +matchbox2.price);
+		System.out.println("Matchbox size:" +matchbox2.size);
+		System.out.println("Matchbox weight:" +matchbox2.weight);
+		System.out.println("Matchbox type:" +matchbox2.type);
+		
+		MatchBox matchbox3=new MatchBox("kamal",100,5);
+		System.out.println("Matchbox brand:" +matchbox3.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox3.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox3.length);
+		System.out.println("Matchbox price:" +matchbox3.price);
+		System.out.println("Matchbox size:" +matchbox3.size);
+		System.out.println("Matchbox weight:" +matchbox3.weight);
+		System.out.println("Matchbox type:" +matchbox3.type);
+		
+		MatchBox matchbox4=new MatchBox("kamal",100,5,50.5);
+		System.out.println("Matchbox brand:" +matchbox4.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox4.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox4.length);
+		System.out.println("Matchbox price:" +matchbox4.price);
+		System.out.println("Matchbox size:" +matchbox4.size);
+		System.out.println("Matchbox weight:" +matchbox4.weight);
+		System.out.println("Matchbox type:" +matchbox4.type);
+		
+		MatchBox matchbox5=new MatchBox("kamal",100,5,50.5,5.5);
+		System.out.println("Matchbox brand:" +matchbox5.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox5.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox5.length);
+		System.out.println("Matchbox price:" +matchbox5.price);
+		System.out.println("Matchbox size:" +matchbox5.size);
+		System.out.println("Matchbox weight:" +matchbox5.weight);
+		System.out.println("Matchbox type:" +matchbox5.type);
+		
+		MatchBox matchbox6=new MatchBox("kamal",100,5,50.5,5.5,1);
+		System.out.println("Matchbox brand:" +matchbox6.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox6.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox6.length);
+		System.out.println("Matchbox price:" +matchbox6.price);
+		System.out.println("Matchbox size:" +matchbox6.size);
+		System.out.println("Matchbox weight:" +matchbox6.weight);
+		System.out.println("Matchbox type:" +matchbox6.type);
+		
+		MatchBox matchbox7=new MatchBox("kamal",100,5,50.5,5.5,1,"safety matches");
+		System.out.println("Matchbox brand:" +matchbox7.brand);
+		System.out.println("Matchbox noOfSticks:" +matchbox7.noOfSticks);
+		System.out.println("Matchbox length:" +matchbox7.length);
+		System.out.println("Matchbox price:" +matchbox7.price);
+		System.out.println("Matchbox size:" +matchbox7.size);
+		System.out.println("Matchbox weight:" +matchbox7.weight);
+		System.out.println("Matchbox type:" +matchbox7.type);
+		
+		System.out.println("Ending main in MatchStickMain");
+	}
+}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
